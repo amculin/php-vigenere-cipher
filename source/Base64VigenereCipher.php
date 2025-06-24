9b22863848b44ee79cdaca843182ca7e7eae8b3b
Fahmi Auliya <amixcustomlinux@gmail.com>
2025-06-24 21:10:45 +0700

    [DEV] Add Base64 mode

--- source/Base64VigenereCipher.php
+++ source/Base64VigenereCipher.php
@@ -1,19 +1,21 @@
 <?php
+
 namespace amculin\cryptography\classic;
 
 use amculin\cryptography\classic\enums\ProcessType;
-use amculin\cryptography\classic\exceptions\InvalidBasicException;
+use amculin\cryptography\classic\exceptions\InvalidBase64Exception;
 
 /**
  * This file is the main class for basic vigenere cipher algortithm.
  *
  * @author Fahmi Auliya Tsani <amixcustomlinux@gmail.com>
  *
- * @version 1.1
+ * @version 1.2
  *
  * @psalm-api
  */
-class BasicVigenereCipher extends VigenereCipherBlueprint
+#[\AllowDynamicProperties]
+class Base64VigenereCipher extends VigenereCipherBlueprint
 {
     public function __construct(
         public string $data,
@@ -21,7 +23,7 @@ class BasicVigenereCipher extends VigenereCipherBlueprint
         public string $process = 'encrypt'
     ) {
         $this->process = $process;
-        $this->tabulaRecta = 'abcdefghijklmnopqrstuvwxyz';
+        $this->tabulaRecta = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
         if ($process == ProcessType::ENCRYPT->value) {
             $this->plainText = $data;
@@ -70,22 +72,22 @@ class BasicVigenereCipher extends VigenereCipherBlueprint
     public function isValid(): bool
     {
         try {
-            $pattern = '/^[a-z]*$/';
+            $pattern = '/^[A-Za-z0-9+\/=]+$/';
 
             if (!$this->isValidKey($pattern)) {
-                throw new InvalidBasicException('Key');
+                throw new InvalidBase64Exception('Key');
             }
 
             if ($this->process == ProcessType::ENCRYPT->value) {
                 if (!$this->isValidPlainText($pattern)) {
-                    throw new InvalidBasicException('Plain text');
+                    throw new InvalidBase64Exception('Plain text');
                 }
             } else {
                 if (!$this->isValidCipherText($pattern)) {
-                    throw new InvalidBasicException('Cipher text');
+                    throw new InvalidBase64Exception('Cipher text');
                 }
             }
-        } catch(InvalidBasicException $e) {
+        } catch (InvalidBase64Exception $e) {
             echo $e->errorMessage();
 
             return false;
